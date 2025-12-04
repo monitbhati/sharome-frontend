d9c8d564f6d1fdfdaa9cfe5f7f58971a994e48be
monitbhati <monit.bhati786@gmail.com>
2025-12-05 01:35:46 +0530

    hero3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
-import heroImage2Image from './assets/heroImage2.jpeg'
+import heroImage3Image from './assets/heroImage3.jpeg'
 
 function App() {
   useEffect(() => { document.title = "Sharomé | Modern Ethnic"; }, []);
@@ -50,7 +50,7 @@ function App() {
             position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 0
         }}>
             <img 
-              src={heroImage2Image} 
+              src={heroImage3Image} 
               alt="Sharome Model" 
               style={{
                 width: '100%', height: '100%', objectFit: 'cover', 
